70bf1e5ff3eccbb560f2b28e2cc00ffbb3b44727
Pavel <pavka.kis@yandex.ru>
2021-10-19 11:30:29 +0500

    homevork Lesson-3

---
+++ Lesson-3/Lesson-3 task-1-2.py
@@ -0,0 +1,26 @@
+NUMERALS = {
+    'one': 'один',
+    'two': 'два',
+    'three': 'три',
+    'four': 'четыре',
+    'five': 'пять',
+    'six': 'шесть',
+    'seven': 'семь',
+    'eight': 'восемь',
+    'nine': 'девять',
+    'ten': 'десять'
+}
+
+
+def num_translate_adv(num):
+
+    for en, rus in NUMERALS.items():
+        if en == num:
+            return rus
+        elif en.title() == num:
+            return rus.title()
+
+
+print(num_translate_adv('Seven'))
+print(num_translate_adv('ten'))
+print(num_translate_adv('10'))
---
+++ Lesson-3/Lesson-3 task-3.py
@@ -0,0 +1,23 @@
+def thesaurus(*args):
+    dictionary_names = {}
+    list_letters = []
+
+    # Этот блок чтобы исключить повторение
+
+    for name in args:
+        list_letters.append(name[0])
+    list_letters = list(set(list_letters))
+    list_letters.sort()
+
+    # Теперь не будет "дублирующих" итераций
+
+    for let in list_letters:
+        list_names = []
+        for name in args:
+            if name[0] == let:
+                list_names.append(name)
+        dictionary_names[let] = list_names
+    return dictionary_names
+
+names = ['Иван', 'Марина', 'Пётр', 'Саня', 'Илья', 'Паша', 'Марина']
+print(thesaurus(*names))
